9b5498f12558ac6f36d7d3f6a5c93d30d33717a8
garywei944 <33930674+garywei944@users.noreply.github.com>
2025-01-20 20:12:53 -0800

    Add lenet.py

--- lenet.py
+++ lenet.py
@@ -1,36 +1,109 @@
 import jax
 from jax import numpy as jnp, random as jrandom
+from jax import jit, vmap, grad, value_and_grad
 from flax import linen as nn
+import equinox as eqx
+
+from jaxtyping import Array, Float, jaxtyped
+from beartype import beartype as typechecker
 
 
-class LeNet(nn.Module):
-    @nn.compact
+class LeNetFlaxNative(nn.Module):
+    def setup(self):
+        self.conv1 = nn.Conv(features=6, kernel_size=(5, 5))
+        self.conv2 = nn.Conv(features=16, kernel_size=(5, 5))
+        self.dense1 = nn.Dense(features=120)
+        self.dense2 = nn.Dense(features=84)
+        self.dense3 = nn.Dense(features=10)
+
+    # @eqx.filter_jit
+    # @jit
+    # def __call__(self, x: Float[Array, "b 32 32 3"]) -> Float[Array, "b 10"]:
     def __call__(self, x):
-        x = nn.Conv(features=6, kernel_size=(5, 5))(x)
+        # conv1
+        x = self.conv1(x)
         x = nn.relu(x)
         x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
 
-        x = nn.Conv(features=16, kernel_size=(5, 5))(x)
+        # conv2
+        x = self.conv2(x)
         x = nn.relu(x)
         x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
 
+        # linear
         x = x.reshape((x.shape[0], -1))
-        x = nn.Dense(features=120)(x)
+        x = self.dense1(x)
         x = nn.relu(x)
-
-        x = nn.Dense(features=84)(x)
+        x = self.dense2(x)
         x = nn.relu(x)
-
-        x = nn.Dense(features=10)(x)
-        x = nn.log_softmax(x)
+        x = self.dense3(x)
 
         return x
 
 
+class LeNetFlax(nn.Module):
+    # @jit
+    # @jaxtyped(typechecker)
+    @nn.compact
+    def __call__(self, x: Float[Array, "b 32 32 3"]) -> Float[Array, "b 10"]:
+        # conv1
+        x = nn.Conv(features=6, kernel_size=(5, 5))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+
+        # conv2
+        x = nn.Conv(features=16, kernel_size=(5, 5))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+
+        # linear
+        x = x.reshape((x.shape[0], -1))
+        x = nn.Dense(features=120)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=84)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=10)(x)
+
+        return x
+
+
+class LeNetEqx(eqx.Module):
+    cnn: eqx.nn.Sequential
+
+    def __init__(self, key: jrandom.PRNGKey):
+        # keys = jrandom.split(key, 5)
+        key1, key2, key3, key4, key5 = jrandom.split(key, 5)
+        self.cnn = eqx.nn.Sequential([
+            eqx.nn.Conv2d(3, 6, (5, 5), key=key1),
+            eqx.nn.Lambda(jax.nn.relu),
+            eqx.nn.MaxPool2d((2, 2), (2, 2)),
+            eqx.nn.Conv2d(6, 16, (5, 5), key=key2),
+            eqx.nn.Lambda(jax.nn.relu),
+            eqx.nn.MaxPool2d((2, 2), (2, 2)),
+            eqx.nn.Lambda(jnp.ravel),
+            eqx.nn.Linear(16 * 5 * 5, 120, key=key3),
+            eqx.nn.Lambda(jax.nn.relu),
+            eqx.nn.Linear(120, 84, key=key4),
+            eqx.nn.Lambda(jax.nn.relu),
+            eqx.nn.Linear(84, 10, key=key5)
+        ])
+
+    def __call__(self, x: Float[Array, "b 32 32 3"]) -> Float[Array, "b 10"]:
+        return self.cnn(x)
+
+
 if __name__ == '__main__':
     key = jrandom.PRNGKey(0)
-    x = jrandom.normal(key, (1, 28, 28, 1))
-    model = LeNet()
-    params = model.init(key, x)['params']
-    y = model.apply({'params': params}, x)
+    x = jrandom.normal(key, (20, 32, 32, 3))
+    # x = jrandom.normal(key, (20, 3, 32, 32))
+    model = LeNetFlaxNative()
+    # model = LeNetFlax()
+    # model = LeNetEqx(key)
+    # # jax.tree_map(lambda x: print(type(x)), model)
+    # print(model)
+    print(model.tabulate(key, x))
+    params = model.init(key, x)
+    y = model.apply(params, x)
     print(y.shape)
+    # print(vmap(model)(x))
+    pass
